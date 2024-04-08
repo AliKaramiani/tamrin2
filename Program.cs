6b374c1db3bf6f8ccef3905433c5b7f49fff3870
Alisdghdjsksj <karamianiali99@gmail.com>
2024-04-08 19:10:26 +0330

    e1

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Vazn x=new Vazn();
+x.weight(76);
+int result =x.weight(76);
+System.Console.WriteLine(result);
---
+++ Vazn.cs
@@ -0,0 +1,9 @@
+public class Vazn {
+public int weight(int kg)
+{
+    int geram = kg*1000;
+    return geram;
+}
+
+
+}
